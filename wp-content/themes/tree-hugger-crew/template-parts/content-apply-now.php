53f88895b0de3d39d03deec4c955f1f52fe86ced
aadityageek435 <aadityageek435@gmail.com>
2023-09-01 18:38:37 +0530

    EOD - 1-9-23

---
+++ wp-content/themes/tree-hugger-crew/template-parts/content-apply-now.php
@@ -0,0 +1,29 @@
+<!-- Banner Start -->
+<section class="main-banner inner-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="banner-content">
+                    <h1 class="h1-title"><?php the_title(); ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Banner End -->
+
+<!-- Application Form Start -->
+<div class="inner-page-text">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="inner-page-box">
+                    <div class="contact-form">
+                        <?php echo do_shortcode('[contact-form-7 id="a71a80f" title="Application Form"]'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Application Form End -->
--- wp-content/themes/tree-hugger-crew/template-parts/content-home.php
+++ wp-content/themes/tree-hugger-crew/template-parts/content-home.php
@@ -1,14 +1,16 @@
 <!-- banner-start -->
 <section class="main-banner back-img" style="background-image: url(<?php echo home_url(); ?>/wp-content/uploads/2023/09/banner-bg.jpg);">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="banner-content">
-                    <h1 class="h1-title">We Cut Because We Care</h1>
-                    <h3 class="h3-title">Central Iowa’s Premier Tree Service</h3>
-                    <div class="banner-btn">
-                        <a href="javascript:void(0);" title="Get a Free Quote" class="sec-btn">Get a Free Quote</a>
-                        <a href="javascript:void(0);" title="Call (515) 520-0971" class="sec-btn  white-outline-btn">Call &nbsp;<span class="for-des callus">(515) 520-0971</span><span class="for-mob">Now!</span></a>
+    <div class="sec-wp">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="banner-content">
+                        <h1 class="h1-title">We Cut Because We Care</h1>
+                        <h3 class="h3-title">Central Iowa’s Premier Tree Service</h3>
+                        <div class="banner-btn">
+                            <a href="javascript:void(0);" title="Get a Free Quote" class="sec-btn">Get a Free Quote</a>
+                            <a href="javascript:void(0);" title="Call (515) 520-0971" class="sec-btn  white-outline-btn">Call &nbsp;<span class="for-des callus">(515) 520-0971</span><span class="for-mob">Now!</span></a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -16,3 +18,92 @@
     </div>
 </section>
 <!-- banner-end -->
+
+<!-- about-start -->
+<section class="sec-about-us">
+    <div class="sec-wp">
+        <div class="about-path">
+            <img src="<?php echo home_url(); ?>/wp-content/themes/tree-hugger-crew/assets/images/about-path.svg" alt="about-path" width="1675" height="630">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="about-brand">
+                        <img src="<?php echo home_url(); ?>/wp-content/uploads/2023/09/about-brand.png" alt="about-brand" width="170" height="170">
+                    </div>
+                    <div class="about-us-img">
+                        <div class="back-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2023/09/about-img.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-6 align-self-center">
+                    <div class="about-us-content">
+                        <h2 class=" h2-title">About Us</h2>
+                        <h3 class="h3-title">We Work on Behalf of Your Tree </h3>
+                        <p>At TreeHugger Complete Tree Care, we are a local family owned business that upholds honesty, integrity, and shows the utmost respect and care for your property. Our tree pruning and removal services are tailored to meet the unique needs of both homeowners and business owners. We take pride in the high standard of our work, and we are committed to maintaining the health and wellness of your trees and property.</p>
+                        <a href="javascript:void(0);" class="" title="About Us, Learn More">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="about-box">
+                        <h3 class="h3-title">Experience</h3>
+                        <p>Certified Arborists serving the Ankeny and Des Moines Area since 2014.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="about-box">
+                        <h3 class="h3-title">Stewardship</h3>
+                        <p>We work honestly, are committed to safety, respect for homeowners and the properties and trees we work with.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="about-box">
+                        <h3 class="h3-title">Service</h3>
+                        <p>Our standard is simple. Exceptional service in professional tree care.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about-end-->
+
+<!-- service-start -->
+<section class="main-service">
+    <div class="sec-wp">
+        <div class="container">
+            <div class="service-title">
+                <h2 class="h2-title">Our Services</h2>
+                <h3 class="h3-title">We Have a Solution for Your Trees</h3>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="service-tab">
+                        <ul>
+                            <li>Tree Removal</li>
+                            <li>Lot Clearing</li>
+                            <li>Tree Pruning</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-8">
+                    <div class="service-content">
+                        <div class="about-brand">
+                            <img src="<?php echo home_url(); ?>/wp-content/uploads/2023/09/tab-brand.png" alt="tab-brand.png" width="230" height="230">
+                        </div>
+                        <div class="service-img">
+                            <div class="back-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2023/09/service-tab-img.jpg');"></div>
+                        </div>
+                        <h3 class="h3-title">Tree Removal</h3>
+                        <p>TreeHugger utilizes specialized equipment and our expertise to provide tree removal services for any tree, any size, any location. Whether due to storm damage, disease, or your preference, we are here to remove your tree for a clean finish.</p>
+                        <a href="javascript:void(0);" class="" title="About Us, Learn More">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<!-- service-end -->
